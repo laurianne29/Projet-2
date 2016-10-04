520e82cc65e5e763fe735aebc7d41b1532e652a5
Jeremy92390 <jeremy.telga@lepoles.com>
2016-10-04 15:36:50 +0200

    modif js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 window.onload = function(){
 	alert("ok ca marche")
-};	
+	alert("Ou non")
+	alert("bon alors c'est comment la?")
+	alert("Qui paye son grec?")
+};
+
+function afficher_cacher(id)
+{
+    if(document.getElementById(id).style.visibility=="hidden")
+    {
+        document.getElementById(id).style.visibility="visible";
+        document.getElementById('bouton_'+id).innerHTML='Cacher le texte';
+    }
+    else
+    {
+        document.getElementById(id).style.visibility="hidden";
+        document.getElementById('bouton_'+id).innerHTML='Afficher le texte';
+    }
+    return true;
+}
